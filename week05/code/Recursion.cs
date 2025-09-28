09807071d87dd74dad2ed6ae6dfb63abe8c73738
gmaxstores <maxwellnvani@gmail.com>
2025-09-28 20:27:25 +0100

    finished week 5 code assignment

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -15,7 +15,14 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
+        if (n <= 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return n * n + SumSquaresRecursive(n - 1);
+        }
     }
 
     /// <summary>
@@ -40,6 +47,17 @@ public static class Recursion
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
         // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+        for (int i = 0; i < letters.Length; i++)
+        {
+            char c = letters[i];
+            string lettersLeft = letters.Remove(i, 1);
+            PermutationsChoose(results, lettersLeft, size, word + c);
+        }
     }
 
     /// <summary>
@@ -97,9 +115,21 @@ public static class Recursion
             return 4;
 
         // TODO Start Problem 3
+        // initialize the remember dictionary if it is null
+        if (remember == null)
+        {
+            remember = new Dictionary<int, decimal>();
+        }
+        // check if the value has already been computed
+        if (remember.ContainsKey(s))
+        {
+            return remember[s];
+        }
 
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+        // store the computed value in the dictionary
+        remember[s] = ways;
         return ways;
     }
 
@@ -119,6 +149,16 @@ public static class Recursion
     public static void WildcardBinary(string pattern, List<string> results)
     {
         // TODO Start Problem 4
+        int index = pattern.IndexOf('*');
+        if (index == -1)
+        {
+            results.Add(pattern);
+            return;
+        }
+        string patternWith0 = pattern[..index] + '0' + pattern[(index + 1)..];
+        string patternWith1 = pattern[..index] + '1' + pattern[(index + 1)..];
+        WildcardBinary(patternWith0, results);
+        WildcardBinary(patternWith1, results);
     }
 
     /// <summary>
@@ -127,17 +167,39 @@ public static class Recursion
     /// </summary>
     public static void SolveMaze(List<string> results, Maze maze, int x = 0, int y = 0, List<ValueTuple<int, int>>? currPath = null)
     {
-        // If this is the first time running the function, then we need
-        // to initialize the currPath list.
-        if (currPath == null) {
-            currPath = new List<ValueTuple<int, int>>();
-        }
-        
+
         // currPath.Add((1,2)); // Use this syntax to add to the current path
 
         // TODO Start Problem 5
         // ADD CODE HERE
+        // If this is the first time running the function, then we need
+        // to initialize the currPath list.
+        currPath ??= [];
 
+        currPath.Add((x, y));
+
+        if (maze.IsEnd(x, y))
+        {
+            results.Add(currPath.AsString());
+            return;
+        }
+
+        // Directions: right, down, left, up
+        int[] dx = [1, 0, -1, 0];
+        int[] dy = [0, 1, 0, -1];
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (maze.IsValidMove(currPath, nx, ny))
+            {
+                // Make a copy of the current path for recursion
+                var newPath = new List<ValueTuple<int, int>>(currPath);
+                SolveMaze(results, maze, nx, ny, newPath);
+            }
+            
+        }
         // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
     }
 }
